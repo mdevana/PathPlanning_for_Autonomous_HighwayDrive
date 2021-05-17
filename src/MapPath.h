53b2792c1986983d7dcc44f299c3a7c128f2bc0f
mdevana <madhusudhan.d@gmail.com>
2021-05-17 16:51:41 +0200

    first test

---
+++ src/MapPath.h
@@ -0,0 +1,31 @@
+#ifndef MapPath_H
+#define MapPath_H
+#include <vector>
+
+
+using std::vector;
+
+class MapPath {
+ public:
+  /**
+   * Constructor
+   */
+  MapPath();
+
+  /**
+   * Destructor.
+   */
+  virtual ~MapPath();
+  
+  void Init();
+  void set_map_path_data(vector<double> x,vector<double> y,vector<double> s,vector<double> dx, vector<double> dy);
+  vector<WayPoint> get_map_path_s(int ind);
+
+ private:
+
+  vector<WayPoint> map_of_highway;
+  
+
+};
+
+#endif  // MapPath_H
