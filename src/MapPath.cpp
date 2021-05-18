0a26a8cd2b0b490a66d490db632e83b760c9d2d7
mdevana <madhusudhan.d@gmail.com>
2021-05-18 05:25:00 +0200

    first test

--- src/MapPath.cpp
+++ src/MapPath.cpp
@@ -21,15 +21,11 @@ MapPath::~MapPath() {}
 void MapPath::set_map_path_data(vector<double> x,vector<double> y,vector<double> s,vector<double> dx, vector<double> dy ) {
   
   int path_size = x.size();
-  WayPoint w_p;
+  
   
   for (int i = 0; i < path_size; ++i) {
 	  
-	  w_p.x_co = x[i];
-	  w_p.y_co = y[i];
-      w_p.s_co = s[i];
-      w_p.dx_co = dx[i];
-      w_p.dy_co = dy[i];
+	  WayPoint w_p(x[i],y[i],s[i],dx[i],dy[i]);
 	  map_of_highway.push_back(w_p);
 	  
   }
