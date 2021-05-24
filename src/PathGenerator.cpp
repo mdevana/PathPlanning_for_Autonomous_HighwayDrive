4e3afa0058e355938e33a441e7e14e435fd39a0d
mdevana <madhusudhan.d@gmail.com>
2021-05-24 13:59:51 +0200

    jerk opt

--- src/PathGenerator.cpp
+++ src/PathGenerator.cpp
@@ -200,6 +200,7 @@ void PathGenerator::generate_map_path(){
 
 void PathGenerator::generate_map_path_JMT(){
 	
+	lanecode followlane=left;
 	double pos_x;
 	double pos_y;
 	double angle;
@@ -285,7 +286,7 @@ void PathGenerator::generate_map_path_JMT(){
 		std::cout <<"End time: " <<end_time<< std::endl;
 		std::cout <<"time increments =" <<time_inc<< std::endl;
 
-		vector<WayPoint> current_wp_points = highway_map.get_map_convertedSD_for_XY_jerk_optimised(s_start, s_end, d_start, d_end, start_time, end_time, time_inc);
+		vector<WayPoint> current_wp_points = highway_map.get_map_convertedSD_for_XY_jerk_optimised(s_start, s_end, d_start, d_end, start_time, end_time, time_inc, followlane);
 		//vector<double> &s_start,vector<double> &s_end, vector<double> &d_start, vector<double> &d_end, double start_time, double start_time, double end_time, double inc
 		
 		
