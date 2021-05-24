19b12ae40ac2215b24d695acd20106522bcc44bf
mdevana <madhusudhan.d@gmail.com>
2021-05-24 15:54:10 +0200

    jerk opt

--- src/PathGenerator.cpp
+++ src/PathGenerator.cpp
@@ -103,7 +103,7 @@ void PathGenerator::generate_circular_path(){
 
 void PathGenerator::generate_map_path(){
 	
-	lanecode followlane=right;
+	lanecode followlane=middle;
 	double pos_x;
 	double pos_y;
 	double angle = (car_yaw) * M_PI / 180;
