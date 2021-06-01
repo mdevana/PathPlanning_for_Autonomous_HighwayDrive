ee1e07ea1e376364e899298f555c2379dd40b8e0
mdevana <madhusudhan.d@gmail.com>
2021-06-01 12:57:52 +0200

    vehicle class

--- src/PathGenerator.cpp
+++ src/PathGenerator.cpp
@@ -221,6 +221,64 @@ void PathGenerator::generate_map_path_with_traffic(vector<vector<double>> sensor
 		
 	}
 	
+	if ( path_size < 2){
+		
+		// 2 points that makes path tangent to the car
+		pts_x.push_back(car_x - cos(car_yaw));
+		pts_y.push_back(car_y - sin(car_yaw));
+		
+		pts_x.push_back(car_x);
+		pts_y.push_back(car_y);
+		
+	}
+	else {
+		
+		
+		
+		// Reference pt is two last point in the Q
+		ref_x = previous_path_x[path_size -1];
+		ref_y = previous_path_y[path_size -1];
+		
+		double ref_x_1 = previous_path_x[path_size -2];
+		double ref_y_1 = previous_path_y[path_size -2];
+		
+		ref_yaw = atan2( ref_y - ref_y_1, ref_x - ref_x_1);
+		
+		pts_x.push_back(ref_x_1);
+		pts_x.push_back(ref_x);
+		
+		pts_y.push_back(ref_y_1);
+		pts_y.push_back(ref_y);
+
+		
+	}
+	
+	std::cout <<"End S  =" <<end_s << std::endl;
+	highway_map.calculate_map_XYspline_for_s(end_s, 6, pts_x, pts_y,ref_yaw);
+		
+	
+	
+	double x_estimate = 30;
+	double y_estimate = highway_map.get_y_from_curve(x_estimate);
+	double dist_estimate = sqrt(x_estimate * x_estimate + y_estimate * y_estimate);
+	
+	double n_dist_inc = dist_estimate / (0.02*max_velocity);
+	double dist_inc_x = x_estimate / n_dist_inc;
+	double x_pt = 0;
+	double y_pt;
+	
+	for(int i = 1; i<= 50 - path_size;i++){
+		
+		x_pt += dist_inc_x;
+		y_pt = highway_map.get_y_from_curve(x_pt);
+
+		
+		next_x_vals.push_back(ref_x + (x_pt * cos(ref_yaw)  - y_pt * sin(ref_yaw)));
+		next_y_vals.push_back(ref_y + (x_pt * sin(ref_yaw)  + y_pt * cos(ref_yaw)));
+
+		
+	}
+	
 		
 	
 	
