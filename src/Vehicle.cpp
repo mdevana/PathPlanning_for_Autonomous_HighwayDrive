f015dad69c3a29adaae44d9300d1e971652c8f34
mdevana <madhusudhan.d@gmail.com>
2021-06-02 05:41:40 +0200

    vehicle class

--- src/Vehicle.cpp
+++ src/Vehicle.cpp
@@ -136,11 +136,8 @@ bool Vehicle::get_vehicle_ahead(map<int, Vehicle> &predictions,
   for (map<int, Vehicle>::iterator it = predictions.begin(); 
        it != predictions.end(); ++it) {
     temp_vehicle = it->second;
-	/*std::cout <<"vehicle ahead s  =" <<temp_vehicle.s<< std::endl;
-	std::cout <<"vehicle ahead lane =" <<temp_vehicle.lane<< std::endl;
-	std::cout <<"ego vehicle s  =" <<this->s<< std::endl;
-	std::cout <<"ego vehicle lane =" <<this->lane<< std::endl;*/
-    if (temp_vehicle.lane == this->lane && temp_vehicle.s > this->s ) {
+//    if (temp_vehicle.lane == this->lane && temp_vehicle.s > this->s ) {
+	  if (temp_vehicle.lane == this->lane && (temp_vehicle.s - this->s < 50) ) {
       min_s = temp_vehicle.s;
       rVehicle = temp_vehicle;
       found_vehicle = true;
