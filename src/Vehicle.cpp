9132d6a72416f696c0a76fbd59031b20057a3dae
mdevana <madhusudhan.d@gmail.com>
2021-10-10 16:41:38 +0200

    test

--- src/Vehicle.cpp
+++ src/Vehicle.cpp
@@ -289,7 +289,7 @@ vector<float> Vehicle::get_kinematics(map<int, Vehicle> &predictions,
 		
     } else {
 	  // Ego has vehicle only in front and no vehilce in rear. Adjust speed to follow the front vehicle by either closing gap or follow vehicle ahead speed
-	  std::cout<< " allowed_gap_to_front_vehicle "<<allowed_gap_to_front_vehicle<<std::endl;
+	  //std::cout<< " allowed_gap_to_front_vehicle "<<allowed_gap_to_front_vehicle<<std::endl;
       	  
       if (allowed_gap_to_front_vehicle > 0 ){	  
 		 
@@ -320,9 +320,10 @@ vector<float> Vehicle::get_kinematics(map<int, Vehicle> &predictions,
 	  }
 	  else {
 		  // if allowed gap tp front vehicle is critical then reduce speed or follow front vehicle
-
+		  std::cout<< " allowed_gap_to_front_vehicle negative "<<allowed_gap_to_front_vehicle<<std::endl; 	
 		  if (min_velocity_accel_limit > vehicle_ahead.v)
 				new_velocity = this->v - 9 * time_span;
+			    std::cout<< " Full slamming of brakes "<<new_velocity<<std::endl;
 			else
 				new_velocity = vehicle_ahead.v;
 			
