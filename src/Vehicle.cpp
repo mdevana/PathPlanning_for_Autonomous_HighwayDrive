e74b88e68a0fc9669d84b8a81b8a1ae4d8432212
mdevana <madhusudhan.d@gmail.com>
2021-10-11 05:44:01 +0200

    test

--- src/Vehicle.cpp
+++ src/Vehicle.cpp
@@ -320,7 +320,7 @@ vector<float> Vehicle::get_kinematics(map<int, Vehicle> &predictions,
 		  // if allowed gap tp front vehicle is critical then reduce speed or follow front vehicle
 		  //std::cout<< " allowed_gap_to_front_vehicle negative "<<allowed_gap_to_front_vehicle<<std::endl;
 		  new_velocity = min_velocity_accel_limit;
-		  if (min_velocity_accel_limit > vehicle_ahead.v){
+		  if (min_velocity_accel_limit > vehicle_ahead.v)
 				new_velocity = vehicle_ahead.v;
 			
 	  }
